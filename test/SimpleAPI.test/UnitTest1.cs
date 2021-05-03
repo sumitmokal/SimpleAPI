dea16589a425c4bc63822d42d24ed8fc5459fcbb
sumit mokal <sumitmokal@gmail.com>
2021-05-03 11:54:52 +0530

    intiate

---
+++ test/SimpleAPI.test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.test
+{
+    ValuesController controller = new ValuesController();
+    public class UnitTest1
+    {
+        ValuesControllers = 
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
